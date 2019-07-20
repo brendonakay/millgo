8856dab723a966ad8406fbe366f77466ec307e65
Brendon Kay <bak04280@gmail.com>
2019-07-19 21:19:11 -0600

    README and main

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Go implementation of Mill
 
 ### TODO
- - Scanner that reads CSV
- - YAML parser
- - One proof of concept field operation
- - One model for proof of concept file type
+ - [] Arg parser
+ - [] Scanner that reads CSV
+ - [] YAML parser
+ - [] One proof of concept field operation
+ - [] One model for proof of concept file type
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+    "log"
+	"os"
+)
+
+func main() {
+
+    // Open file
+	fileHandle, err := os.Open("/Users/brendon/workspace/tmp/Jun-26-00_00_00-05_59_59-2019.psvlog")
+    if err != nil {
+        log.Fatal(err)
+        defer fileHandle.Close()
+    }
+    // Initialize scanner
+	fileScanner := bufio.NewScanner(fileHandle)
+
+    // Read out scanner
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+}
