de2aa115bc52797d364922b15d31c7e22d453acc
Brendon Kay <bak04280@gmail.com>
2019-07-28 13:56:36 -0600

    Working goroutine that reads in file and unmarshalls csv to AuditLog

--- main.go
+++ main.go
@@ -13,14 +13,52 @@ import (
 	"millgo/packages"
 )
 
+// Process CSV lines to struct
+// TODO: Can this be a goroutine? Why, yes it can. In fact, it might
+//  work best as a generator function that returns a receive only chan.
+// Perhaps implement a Select?
+func stageOneChan(reader *csv.Reader, yamlConfig millgo.YamlConfig) (
+	<-chan millgo.AuditLog, <-chan bool) {
+	// Channel sending data to initial transform stage
+	stageOne := make(chan millgo.AuditLog)
+	endOfFile := make(chan bool)
+
+	go func() {
+		for {
+			line, err := reader.Read()
+			fmt.Printf("--- csv line:\n%s\n\n", line)
+			if err == io.EOF {
+				fmt.Printf("END OF FILE")
+				close(stageOne)
+				endOfFile <- true
+				close(endOfFile)
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			auditLogStruct := millgo.AuditLog{
+				Evidence:     millgo.EVIDENCE,
+				AuditLog:     millgo.AUDIT_LOG,
+				Timestamp:    line[yamlConfig.AuditLog.Timestamp],
+				PatientId:    line[yamlConfig.AuditLog.PatientId],
+				EmployeeId:   line[yamlConfig.AuditLog.EmployeeId],
+				AccessAction: line[yamlConfig.AuditLog.AccessAction],
+			}
+			stageOne <- auditLogStruct
+		}
+	}()
+	return stageOne, endOfFile
+}
+
 func main() {
-	// Parse YAML
+	// Retrieve YAML file
 	yamlBin, err := os.Open("data/example.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer yamlBin.Close()
 
+	// Read YAML file
 	yamlFile, err := ioutil.ReadAll(yamlBin)
 	if err != nil {
 		log.Fatal(err)
@@ -28,9 +66,8 @@ func main() {
 
 	fmt.Printf("%s", yamlFile)
 
-	//yaml := parseYaml(yamlFile)
+	// Parse YAML file to config struct
 	yamlConfig := millgo.YamlConfig{}
-	//yamlConfig := make(map[interface{}]interface{})
 
 	err = yaml.Unmarshal(yamlFile, &yamlConfig)
 	if err != nil {
@@ -38,7 +75,6 @@ func main() {
 	}
 
 	fmt.Printf("--- yaml:\n%v\n\n", yamlConfig)
-
 	fmt.Printf("%v", yamlConfig.AuditLog.Timestamp)
 
 	// Open file
@@ -51,48 +87,16 @@ func main() {
 	// Init CSV File reader
 	csvReader := csv.NewReader(fileHandle)
 
-	// Channel sending data to initial transform stage
-	stageOne := make(chan millgo.AuditLog)
-	endOfFile := make(chan bool)
+	// Get stageOne channel
+	consumer, eof := stageOneChan(csvReader, yamlConfig)
 
-	// Process CSV lines to struct
-	// TODO: Can this be a goroutine? Why, yes it can. In fact, it might
-	//  work best as a generator function that returns a receive only chan.
-	// Perhaps implement a Select?
+	// Read output from stageOne channel
 	for {
-		line, err := csvReader.Read()
-		fmt.Printf("--- csv line:\n%s\n\n", line)
-		if err == io.EOF {
-			fmt.Printf("END OF FILE")
-			endOfFile <- true
-			break
-		} else if err != nil {
-			log.Fatal(err)
+		select {
+		case s := <-consumer:
+			fmt.Println(s)
+		case <-eof:
+			return
 		}
-		auditLogStruct := millgo.AuditLog{
-			Evidence:     millgo.EVIDENCE,
-			AuditLog:     millgo.AUDIT_LOG,
-			Timestamp:    line[yamlConfig.AuditLog.Timestamp],
-			PatientId:    line[yamlConfig.AuditLog.PatientId],
-			EmployeeId:   line[yamlConfig.AuditLog.EmployeeId],
-			AccessAction: line[yamlConfig.AuditLog.AccessAction],
-		}
-		stageOne <- auditLogStruct
 	}
-
-	go func() {
-		for{
-			select {
-			case s := <-stageOne:
-				fmt.Println(s)
-			case <-endOfFile:
-				break
-			}
-		}
-	}()
-	// This will be the first transform stage. Receiving into Stage 1
-	//for {
-	//	fmt.Println(<-stageOne)
-	//}
-	//wg.Wait()
 }
