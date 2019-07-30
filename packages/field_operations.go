11a6c15d5a39ddca30f76b87af7f332ea1ec41eb
Brendon Kay <bak04280@gmail.com>
2019-07-29 20:50:01 -0600

    wip

---
+++ packages/field_operations.go
@@ -0,0 +1,15 @@
+package millgo
+
+/* TODO
+- Inspiration coming from https://stackoverflow.com/questions/6769020/go-map-of-functions
+FIELD_OPS = {
+    'constant': _use_constant,
+    'date_fmt': _change_date_format,
+    'map': _use_mapped_value,
+    'remove': _remove_from_value,
+    'substitute': _substitute_in_value,
+    'truncate': _truncate_value
+}
+*/
+
+
